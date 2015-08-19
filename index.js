cd5e7557cbca031519399525cd1a2519fb9e5192
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-19 07:28:52 -0400

    clean up, add docs

--- index.js
+++ index.js
@@ -25,119 +25,123 @@ lazy('write');
 function FetchFiles(options) {
   this.options = options || {};
   this.presets = {};
-  this._queue = [];
+  this.urls = [];
 }
 
-/**
- * Store a preset with the given configuration values.
- *
- * @param {String} `name` Preset name, e.g. `github`
- * @param {Object} `config`
- * @return {Object} Returns `FetchFiles` for chaining
- * @api public
- */
+FetchFiles.prototype = {
+  constructor: FetchFiles,
 
-FetchFiles.prototype.preset = function(name, config) {
-  if (arguments.length === 1) {
-    return this.presets[name];
-  }
-  this.presets[name] = config;
-  return this;
-};
+  /**
+   * Store a preset with the given configuration values.
+   *
+   * ```js
+   * downloader.preset('github', {
+   *   url: 'https://raw.githubusercontent.com',
+   *   fn: function (preset, config) {
+   *     config.pathname = config.url.split('/').slice(-3).join('_');
+   *     return preset.url;
+   *   }
+   * });
+   * ```
+   * @name .preset
+   * @param {String} `name` Preset name, e.g. `github`
+   * @param {Object} `config`
+   * @return {Object} Returns `FetchFiles` for chaining
+   * @api public
+   */
 
-/**
- * Add a URL to the download queue.
- *
- * @param {String} `url`
- * @param {Object} `config` The configuration to use for the URL.
- * @return {Object} Returns `FetchFiles` for chaining
- * @api public
- */
-
-FetchFiles.prototype.queue = function(url, config) {
-  this._queue.push(lazy.extend({url: url}, config));
-  return this;
-};
-
-/**
- * Normalize configuration values.
- *
- * @param {Object} `config` The configuration value to normalize.
- * @return {Object} normalized config object.
- */
-
-FetchFiles.prototype.config = function(config) {
-  var opts = lazy.extend({}, this.options, config);
-  opts.destBase = opts.destBase || process.cwd();
-
-  if (opts.preset && this.presets[opts.preset]) {
-    var preset = this.presets[opts.preset];
-
-    // if `fn` is defined, use it to resolve the URL to download
-    if (typeof preset.fn === 'function') {
-      var base = preset.fn.call(this, lazy.omit(preset, 'fn'), opts);
-      opts.url = url.resolve(base, opts.url);
+  preset: function(name, config) {
+    if (arguments.length === 1) {
+      return this.presets[name];
     }
-  }
-  // resolve the destination filepath
-  opts.dest = path.resolve(process.cwd(), opts.destBase, opts.pathname);
-  return opts;
-};
+    this.presets[name] = config;
+    return this;
+  },
 
-/**
- * Set the dest filepath to use.
- *
- * @param {String} `fp`
- * @return {Object}
- */
+  /**
+   * Add a URL to the download queue.
+   *
+   * ```js
+   * downloader.queue('/assemble/assemble/v0.6.0/.verb.md', {preset: 'github'});
+   * ```
+   * @name .queue
+   * @param {String} `url`
+   * @param {Object} `config` The configuration to use for the URL.
+   * @return {Object} Returns `FetchFiles` for chaining
+   * @api public
+   */
 
-FetchFiles.prototype.dest = function(fp) {
-  this.options.pathname = fp;
-  return this;
-};
+  queue: function(url, config) {
+    this.urls.push(lazy.extend({url: url}, config));
+    return this;
+  },
 
-/**
- * Write a file to disk.
- *
- * @param {String} `fp`
- * @return {Object}
- */
+  /**
+   * Normalize configuration values.
+   *
+   * @name .config
+   * @param {Object} `config` The configuration value to normalize.
+   * @return {Object} normalized config object.
+   */
 
-FetchFiles.prototype.write = function(fp) {
-  lazy.write.stream(fp);
-  return this;
-};
+  config: function(config) {
+    var opts = lazy.extend({}, this.options, config);
+    opts.destBase = opts.destBase || process.cwd();
 
-/**
- * Fetch all files in the queue.
- *
- * @param {Function} `cb` callback
- * @return {Object} Returns `FetchFiles` for chaining
- * @api public
- */
+    if (opts.preset && this.presets[opts.preset]) {
+      var preset = this.presets[opts.preset];
 
-FetchFiles.prototype.fetch = function(cb) {
-  if (typeof cb !== 'function') {
-    cb = function noop () {};
-  }
-
-  var queue = this._queue;
-  this._queue = [];
-
-  lazy.each(queue, function (options, next) {
-    var config = this.config(options);
-    if (!config.dest) {
-      next(new Error('fetch-files needs a dest path.'));
-      return;
+      // if `fn` is defined, use it to resolve the URL to download
+      if (typeof preset.fn === 'function') {
+        var base = preset.fn.call(this, lazy.omit(preset, 'fn'), opts);
+        opts.url = url.resolve(base, opts.url);
+      }
     }
-    if (!config.url) {
-      next(new Error('fetch-files needs a url.'));
-      return;
+    // resolve the destination filepath
+    opts.dest = path.resolve(process.cwd(), opts.destBase, opts.pathname);
+    return opts;
+  },
+
+  /**
+   * Fetch all files in the queue.
+   *
+   * ```js
+   * downloader.fetch(function (err, res) {
+   *   console.log(res);
+   * });
+   * ```
+   * @name .fetch
+   * @param {Function} `cb` callback
+   * @return {Object} Returns `FetchFiles` for chaining
+   * @api public
+   */
+
+  fetch: function(cb, done) {
+    if (arguments.length === 1) {
+      done = cb;
+      cb = function noop() {};
     }
-    config.contents = lazy.request(config);
-    next(null, config);
-  }.bind(this), cb);
-  return this;
+
+    var queue = this.urls;
+    this.urls = [];
+
+    lazy.each(queue, function (options, next) {
+      var config = this.config(options);
+      if (!config.dest) {
+        next(new Error('fetch-files needs a dest path.'));
+        return;
+      }
+      if (!config.url) {
+        next(new Error('fetch-files needs a url.'));
+        return;
+      }
+
+      config.contents = lazy.request(config);
+      cb(config);
+      next(null, config);
+    }.bind(this), done);
+    return this;
+  }
 };
 
 /**
@@ -152,20 +156,6 @@ FetchFiles.extend = function(Ctor) {
   util.inherits(Ctor, FetchFiles);
   lazy.extend(Ctor, FetchFiles);
 };
-
-/**
- * Utils
- */
-
-function isFunction (val) {
-  return typeof val === 'function';
-}
-
-function isObject (val) {
-  return val && typeof val === 'object'
-    && typeof val !== 'function';
-}
-
 
 /**
  * Expose `FetchFiles`
